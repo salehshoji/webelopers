cce104ae33e8471113f7e6a6ccbdd9767f84e2ae
salehshoji <mohamadsaleh.shojai@yahoo.com>
2021-03-04 12:03:15 +0330

    add djnago user url

--- emptyLamp/urls.py
+++ emptyLamp/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from main.views import get_main
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', get_main, name='home')
+    path('', get_main, name='home'),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
