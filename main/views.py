bbca80637ffe74f2986bcbd4549dd9fa66aa6642
mtndaghyani <matin.daghyani@gmail.com>
2021-03-05 09:56:17 +0330

    Show both errors in signup view if exist

--- main/views.py
+++ main/views.py
@@ -1,9 +1,6 @@
 from django.contrib import messages
 from django.contrib.auth.models import User
-from django.http import HttpResponseBadRequest
 from django.shortcuts import render, redirect
-
-from main.forms import UserForm
 
 
 def home_page_view(request):
@@ -18,20 +15,20 @@ def signup_page_view(request):
         username = request.POST.get('username')
         password1 = request.POST.get('password1')
         password2 = request.POST.get('password2')
+        error = False
         if len(User.objects.filter(username=username)) > 0:
             messages.error(request, 'نام کاربری شما در سیستم موجود است')
-            return redirect('signup-page')
+            error = True
         if password1 != password2:
             messages.error(request, 'گذرواژه و تکرار گذرواژه یکسان نیستند')
+            error = True
+        if error:
             return redirect('signup-page')
+        else:
+            user = User(username=username, email=email, first_name=firstname, last_name=lastname, is_active=True)
+            user.set_password(password1)
 
-        user = User(username=username, email=email, first_name=firstname, last_name=lastname, is_active=True)
-        user.set_password(password1)
-
-        user.save()
-        return redirect('home')
+            user.save()
+            return redirect('home')
     else:
-        data = {
-            'form': UserForm()
-        }
-        return render(request, 'main/signup.html', data)
+        return render(request, 'main/signup.html')
